d789acbd61c626627f46801d68f1c3d8bfa7b74a
kessiafv <kessiafv@gmail.com>
2024-05-09 09:29:20 +0100

    ATT

---
+++ aula 13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 3)
+
+
+// var c = 1
+// while (c <= 3) {
+//     console.log(`Passo ${c}`)
+//     c++ //c recebe c+1
+// }
+
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
---
+++ aula 13/node_80d09da552933.tmp.js
@@ -0,0 +1,19 @@
+var c = 1
+do {
+    console.log('Tudo bem?')
+} while (c <= 3)
+
+
+// var c = 1
+// while (c <= 3) {
+//     console.log(`Passo ${c}`)
+//     c++ //c recebe c+1
+// }
+
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
